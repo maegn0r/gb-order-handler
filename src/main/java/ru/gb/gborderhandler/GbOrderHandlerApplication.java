74de1c4a299d0d377836e51f5d9cd045f66f116a
novitskynick <novitskynick@gmail.com>
2021-12-22 22:15:59 +0300

    Initial commit

---
+++ src/main/java/ru/gb/gborderhandler/GbOrderHandlerApplication.java
@@ -0,0 +1,13 @@
+package ru.gb.gborderhandler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GbOrderHandlerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GbOrderHandlerApplication.class, args);
+    }
+
+}
